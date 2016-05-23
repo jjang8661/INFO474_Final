19e679b9bc73b9e7d8805ed4d85ed0410eca20a9
Woosuk Seo <seow@uw.edu>
2016-05-23 00:51:11 -0700

    Add scroller derivative and update readme

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-## Demo 2: Angular Scroller
+## INFO474 Final Project
 
-This demo demonstrates how to use a scroller to update a reusable chart function within the context of an Angular applicaiton.
-
-The final product looks like this:
-![demo-2 complete](imgs/complete.png)
---
+++ js/app.js
@@ -0,0 +1,57 @@
+
+var myApp = angular.module('myApp', [])
+
+// Main controller
+.controller('MainController', function($scope) {
+     // Data for the chart
+     $scope.data = [
+       {id:0, text:'Paragraph 0'},
+       {id:1, text:'Paragraph 1'},
+       {id:2, text:'Paragraph 2'},
+       {id:3, text:'Paragraph 3'}
+     ];
+
+     // Array of objects that correspond to each step
+     $scope.settings = [
+       {color:'red', fontSize:20, filter:function(d){return d}},
+       {color:'blue', fontSize:10, filter:function(d){return d.id>2}},
+       {color:'orange', fontSize:100, filter:function(d){return d.id<3}},
+       {color:'green', fontSize:30, filter:function(d){return d}},
+     ];
+
+     $scope.step = 0;
+
+     // Text for each section
+     $scope.sectionText = [
+       {text:'Section 0'},
+       {text:'Section 1'},
+       {text:'Section 2'},
+       {text:'Section 3'}
+     ];
+
+     // Desired section height
+     $scope.sectionHeight = 400;
+ })
+
+// Projects controller
+.controller('ProjectsController', function($scope, ProjectData){
+  ProjectData.then(function(data){
+    $scope.projects = data
+  })
+})
+
+// Scroll directive
+.directive("scroll", function ($window) {
+    return {
+      restrict:'E', // this directive is specified as an html element <scroll>
+      scope:false, // use global scope
+      // Create a link function that allows dynamic element creation
+      link:function(scope, elem) {
+          elem.bind("scroll", function() {
+              scope.step = Math.ceil((this.scrollTop - 10)/ scope.sectionHeight);
+              scope.$apply();
+          });
+      }
+    };
+})
+
