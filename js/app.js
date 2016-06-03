a5d117c08d42aa1a17b8a831bdd719d6acb4efa9
t-ha <tjunkha@gmail.com>
2016-06-03 12:39:50 -0700

    dots

--- js/app.js
+++ js/app.js
@@ -3,33 +3,33 @@ var myApp = angular.module('myApp', [])
 // Main controller
 .controller('MainController', function($scope) {
 
+    // Desired section height
+    $scope.sectionHeight = 800;
     $scope.data = [
+      {id: 0, color: '#000000'},
       {id: 1, color: '#000000'},
       {id: 2, color: '#000000'},
-      {id: 3, color: '#000000'}
-      // {id: 4, color: '#000000'}
+      {id: 3, color: '#000000'},
+      {id: 4, color: '#000000'}
     ];
 
      // Array of objects that correspond to each step
      $scope.settings = [
-       {numD: 0, filter:function(d){return d.id == 1}},
+       {numD: 0, filter:function(d){return d.id == 0}},
        {numD: 1, filter:function(d){return d.id == 1}},
        {numD: 27, filter:function(d){return d.id == 2}},
        {numD: 1000, filter:function(d){return d.id == 3}}, //9000 is too big and cause lagging
-       // {numD: 300, filter:function(d){return d.id == 4}}
+       {numD: 1, radius: 8, filter:function(d){return d.id == 4}}
      ];
 
      $scope.step = 0;
      $scope.sectionSet = [
-       {text:'1 person is injured every 53 minutes in a drunk driving incident', color:'#000000'},
-       {text:'27 people are injured or killed per day in a drunk driving incident', color:'#000000'},
-       {text:'9,967 people are either injured or killed each year in a drunk driving incident', color:'#000000'},
-       {text:'Section 3',color:'#000000'}
-       // {text:'Section 4',color:'#000000'}
-     ];
-
-     // Desired section height
-     $scope.sectionHeight = 800;
+       {text:'1 person is injured every 53 minutes in a drunk driving incident', color:'#000000', sh: 800},
+       {text:'27 people are injured or killed per day in a drunk driving incident', color:'#000000', sh: 800},
+       {text:'9,967 people are either injured or killed each year in a drunk driving incident', color:'#000000', sh: 800},
+       {text:'Section 3',color:'#000000', sh: 800},
+       {text:'',color:'#000000', sh: 10}
+     ]
  })
 
 // Projects controller
@@ -49,7 +49,6 @@ var myApp = angular.module('myApp', [])
           angular.element($window).bind("scroll", function() {
               scope.step = Math.ceil(($(window).scrollTop() - $("scroll.scroller").offset()["top"])/scope.sectionHeight);
               scope.$apply();
-         
           });
       }
     };
@@ -68,7 +67,13 @@ var myApp = angular.module('myApp', [])
 
         // Instantiate your chart with given settings
         var numDots = scope.settings[scope.step].numD;
-        var myChart = Dots().numNodes(numDots).color('#DADFE1');
+        var myChart;
+        if (typeof scope.settings[scope.step].radius !== 'undefined') {
+          myChart = Dots().numNodes(numDots).color('#DADFE1').radiusChange(false).radius(scope.settings[scope.step].radius);
+        } else {
+          myChart = Dots().numNodes(numDots).color('#DADFE1');
+        }
+        // var myChart = Dots().numNodes(numDots).color('#DADFE1');
 
         // Get the current data
         var currentData = scope.data.filter(scope.settings[scope.step].filter);
